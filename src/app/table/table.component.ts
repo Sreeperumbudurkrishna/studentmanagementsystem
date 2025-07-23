3576fa60e4e38415ac7579fb71a143a1dcffab93
Krishna Vamshi <Sreeperumbudurkrishna@gmail.com>
2025-07-23 09:53:53 -0500

    first commit

---
+++ src/app/table/table.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent {
+tabledata=[
+  {
+    id:1,
+    name:'Krishna',
+    email:'kv251137@gmail.com',
+    branch:'CSE',
+    mobilenumber:'3125304796',
+  },
+  {
+    id:1,
+    name:'Krishna',
+    email:'kv251137@gmail.com',
+    branch:'CSE',
+    mobilenumber:'3125304796',
+  }
+]
+}
